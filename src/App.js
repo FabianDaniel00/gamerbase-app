e79c9f3b78a70325e28ff03503f452c8d94d7c7c
Fabian Daniel <kiloboggaming@gmail.com>
2020-07-10 15:52:57 +0200

    Push

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.scss';
 import NavigationBar from './components/NavigationBar';
 import Sidebar from './components/Sidebar';
@@ -11,30 +11,32 @@ import { About } from './About';
 import { NoMatch } from './NoMatch';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-function App() {
-    return(
-        <React.Fragment>
-            <Router>
-                <div className="sticky-navbar">
-                    <NavigationBar />
-                </div>
-                <div className="height">
-                    <Sidebar />
-                    <div className="content-wrapper">
-                        <ClassCarousel />
-                        <Switch>
-                            <Route exact path="/" component={ Home } />
-                            <Route path="/login" component={ Login } />
-                            <Route path="/signup" component={ Register } />
-                            <Route path="/about" component={ About } />
-                            <Route component={ NoMatch } />
-                        </Switch>
+class App extends Component {
+    render() {
+        return(
+            <React.Fragment>
+                <Router>
+                    <div className="sticky-navbar">
+                        <NavigationBar />
                     </div>
-                    <Footer />
-                </div>
-            </Router>
-        </React.Fragment>
-    );
+                    <div className="height">
+                        <Sidebar />
+                        <div className="content-wrapper">
+                            <ClassCarousel />
+                            <Switch>
+                                <Route exact path="/" component={ Home } />
+                                <Route path="/login" component={ Login } />
+                                <Route path="/signup" component={ Register } />
+                                <Route path="/about" component={ About } />
+                                <Route component={ NoMatch } />
+                            </Switch>
+                        </div>
+                        <Footer />
+                    </div>
+                </Router>
+            </React.Fragment>
+        );
+    }
 }
 
 export default App;
